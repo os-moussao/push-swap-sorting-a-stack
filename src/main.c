abaedae8b6525693ea4e1342c5207bb652df1826
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-01-04 23:52:36 +0100

    fixed main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <omoussao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 00:45:36 by omoussao          #+#    #+#             */
-/*   Updated: 2022/01/02 16:20:25 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/01/04 19:30:47 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 int	main(int ac, char **av)
 {
 	t_stack	*a;
+
+	if (ac <= 1)
+		return (1);
 
 	a = create(av + 1, ac - 1);
 	sort_stack(a);
